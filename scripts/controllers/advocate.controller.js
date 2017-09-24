85f4e0c27ccd266311b525125f6eb24fd40e6baa
Cat Yanish <catyanish@Cats-MacBook-Air.local>
2017-09-24 10:37:18 -0500

    basic page looks basic

--- scripts/controllers/advocate.controller.js
+++ scripts/controllers/advocate.controller.js
@@ -4,6 +4,23 @@ myApp.controller('AdvController', function($http, $location) {
   console.log('AdvController created');
   var vm = this;
 
-  alert('adv controller');
+  // alert('adv controller');
+  function sendEmail(address) {
+    window.location.href = 'mailto:' + address;
+}
+
+
+  vm.official;
+
+vm.getReps = function(address) {
+    $http.get('https://www.googleapis.com/civicinfo/v2/representatives?key=AIzaSyBlOIeTeyReDkPTNQEL-rt_xe634WuRKSE&address='+encodeURIComponent(address)).then(function(response) {
+      console.log('get reps called');
+      console.log('offical ', response.data);
+
+
+    });
+  }
+  // vm.getReps(address);
+
 
 }); //end of AdvocateController
