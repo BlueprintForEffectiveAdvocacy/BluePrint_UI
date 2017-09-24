cf124f3e801484bab5278b9807e8103409041689
Cat Yanish <catyanish@Cats-MacBook-Air.local>
2017-09-24 11:19:58 -0500

    receiving all data from localhost

--- scripts/controllers/advocate.controller.js
+++ scripts/controllers/advocate.controller.js
@@ -10,17 +10,31 @@ myApp.controller('AdvController', function($http, $location) {
 }
 
 
-  vm.official;
+vm.official;
 
 vm.getReps = function(address) {
     $http.get('https://www.googleapis.com/civicinfo/v2/representatives?key=AIzaSyBlOIeTeyReDkPTNQEL-rt_xe634WuRKSE&address='+encodeURIComponent(address)).then(function(response) {
       console.log('get reps called');
       console.log('offical ', response.data);
-
+      vm.official = response.data;
+      console.log("vm.official", vm.offical);
 
     });
   }
   // vm.getReps(address);
 
+
+
+  vm.getTopic = function() {
+      $http.get('http://172.16.254.125:8000/issues/?format=json').then(function(response) {
+        console.log('get Issue called');
+        console.log('issue ', response);
+
+
+      });
+    }
+
+
+    vm.getTopic('Racial Justice');
 
 }); //end of AdvocateController
