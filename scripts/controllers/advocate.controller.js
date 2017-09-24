12972ca89db9489fdb420a764353d34271c0cfd2
Cat Yanish <catyanish@Cats-MacBook-Air.local>
2017-09-24 12:57:55 -0500

    basic representative setup

--- scripts/controllers/advocate.controller.js
+++ scripts/controllers/advocate.controller.js
@@ -1,5 +1,3 @@
-// var myApp = angular.module('myApp', ['ngRoute']);
-
 myApp.controller('AdvController', function($http, $location) {
   console.log('AdvController created');
   var vm = this;
@@ -10,15 +8,17 @@ myApp.controller('AdvController', function($http, $location) {
 }
 
 
-vm.official;
+
 
 vm.getReps = function(address) {
     $http.get('https://www.googleapis.com/civicinfo/v2/representatives?key=AIzaSyBlOIeTeyReDkPTNQEL-rt_xe634WuRKSE&address='+encodeURIComponent(address)).then(function(response) {
       console.log('get reps called');
       console.log('offical ', response.data);
-      vm.official = response.data;
-      console.log("vm.official", vm.offical);
 
+
+
+      vm.rep = response.data.officials;
+      console.log("vm.rep", vm.rep);
     });
   }
   // vm.getReps(address);
@@ -29,12 +29,8 @@ vm.getReps = function(address) {
       $http.get('http://172.16.254.125:8000/issues/?format=json').then(function(response) {
         console.log('get Issue called');
         console.log('issue ', response);
-
-
       });
     }
-
-
     vm.getTopic('Racial Justice');
 
 }); //end of AdvocateController
